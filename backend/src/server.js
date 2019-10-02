2e38ff7e9e48fe2ad34d20ef27e9d4457c67cb68
Rafael Schneider <rafaelvschneider@gmail.com>
2019-10-01 23:45:51 -0300

    Desenvolvimento inicial da API Rest

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-u56ge.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// GET, POST, PUT, DELETE
+
+// req.query = acessar query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
